2dd5b4ffa74289330768eedb493d181b07e633ee
tgseufert <tseufert01@gmail.com>
2022-03-17 10:41:10 -0400

    ZZ

--- scripts/universalScript.js
+++ scripts/universalScript.js
@@ -1,4 +1,10 @@
 
+const apiURL = 'https://api.themoviedb.org/3/';
+const apiKey = '?api_key=a96991b0bf66d698b2c930defd8ed6db';
+//search pattern is: apiURL+'search/movie'+apiURL+'&query='+ movie_title
+//specific movie pattern is apiURL+movieID+apiKey
+//documentation here: https://developers.themoviedb.org/3/getting-started/introduction
+
 
 const themeStylesheet =document.getElementById("theme");
 const themeToggle =document.getElementById('theme-toggle');
