b7ee518a44aa3c68056156bb74dc1a4aff58a511
tylerdan <tyler546@revature.net>
2022-03-18 13:04:40 -0700

    got the html page linking to the script and stylesheet correctly. Minor syntax correction in login.css. Syntax corrections in login.js

--- scripts/login.js
+++ scripts/login.js
@@ -5,15 +5,21 @@ const loginError = document.getElementById('login-error-msg');
 loginButton.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(this);
+    const searchParams = new URLSearchParams();
+
+    for(const pair of formData){
+        searchParams.append(pair[0], pair[1]);
+    }
+    console.log(searchParams);
     // test
     fetch(/*'probably controller location'*/, {
         method:'post',
-        body: formData
-    })then(function(response) {
+        body: searchParams
+    }).then(function(response) {
         return response.text();
-    })then(function(text){
+    }).then(function(text){
         console.log(text);
-    })catch(function(error){
+    }).catch(function(error){
         console.log(error);
     })
 })
