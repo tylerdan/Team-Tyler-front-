20417c1569aeb9323849ca119a46a775f6e94bd4
tylerdan <tyler546@revature.net>
2022-03-24 10:19:34 -0700

    cleaning up comments

--- scripts/login.js
+++ scripts/login.js
@@ -42,44 +42,6 @@ userForm.addEventListener("submit", async (e) =>{
     }
 })
 
-/*function handleSubmit(event) {
-    event.preventDefault();
-    var user = document.getElementById('userName').value;
-    var pass = document.getElementById('passWord').value;
-    var input = {'userName':user,
-              'passWord':pass
-    };
-    // turns user input into json format for backend
-    console.log(input);
-    const data = JSON.stringify(input);
-    console.log(data);
-    // should post data to backend
-    fetch(url, {
-    method:'POST',
-        body: data,
-        headers: new Headers({
-            'Content-Type': 'application/json'
-        })
-    }).then(response => response.json())
-    .then(function(response) {
-        console.log(response);
-        // checks if response got through
-        if(response.ok) {
-            // if there is an account in the database
-            if(response != null) {
-                // cookie structure: username=[user], lasts for [1] day
-                setCookie("username", user, 1);
-                // changes profile button to say "[user] profile"
-                document.getElementById('profile-button').innerHTML = user + " profile";
-                // redirects to profile page
-                location.assign('../webpages/userPage.html');
-            }
-        }
-    }).catch(function(error){ // if an error is thrown, show it in console
-        console.log(error);
-    })
-}*/
-
 // sets cookie
 function setCookie(cName, userName, exdays){
     const d = new Date();
--- scripts/register.js
+++ scripts/register.js
@@ -43,47 +43,6 @@ registerForm.addEventListener("submit", async (e) =>{
     }
 })
 
-/*function handleSubmit(event) {
-    event.preventDefault();
-    let firstName = document.getElementById('firstName').value;
-    let lastName = document.getElementById('lastName').value;
-    let user = document.getElementById('userName').value;
-    let pass = document.getElementById('passWord').value;
-    let name = {'firstName':firstName,
-                'lastName':lastName
-    };
-    let input = {name,
-              'userName':user,
-              'passWord':pass
-    };
-    // turns user input into json format for backend
-    console.log(input);
-    const data = JSON.stringify(input);
-    console.log(data);
-    // should post data to backend
-    fetch(url, {
-    method:'POST',
-        body: data,
-        headers: new Headers({
-            'Content-Type': 'application/json'
-        })
-    }).then(response => response.json())
-    .then(function(response) {
-        console.log(response);
-        // checks if response got through
-        // redirects user back to userPage.html, sets cookie, changes user profile button
-        //     to "[user] profile"
-        if(response.ok) {
-            console.log("Connected!!!");
-            setCookie("username", user, 1);
-            document.getElementById('profile-button').innerHTML = user + " profile";
-            location.assign('../webpages/userPage.html');
-        }
-    }).catch(function(error){ // if an error is thrown, show it in console
-        console.log(error);
-    })
-}*/
-
 // sets cookie as if user logged in
 function setCookie(cName, userName, exdays){
     const d = new Date();
